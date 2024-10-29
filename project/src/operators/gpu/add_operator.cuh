5439099c1a8302cf137c5c9ade1ddd3739e04a24
Zhang Tianhong <69030077+zhangtianhong-1998@users.noreply.github.com>
2024-10-29 23:16:40 +0800

    Delete project/src/operators/gpu/add_operator.cuh

--- project/src/operators/gpu/add_operator.cuh
+++
@@ -1,9 +0,0 @@
-#ifndef ADD_OPERATOR_CU_H
-#define ADD_OPERATOR_CU_H
-
-namespace cudaoperators
-{
-
-    void add_operator_cu(int* input1, int* input2, int* output, const int size);
-}  // namespace operator
-#endif 
