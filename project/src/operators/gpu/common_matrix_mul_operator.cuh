d4ae7613b2cf63ea03c37e27f1ab571c38d86c32
Zhang Tianhong <69030077+zhangtianhong-1998@users.noreply.github.com>
2024-10-29 23:16:07 +0800

    Delete project/src/operators/gpu/common_matrix_mul_operator.cuh

--- project/src/operators/gpu/common_matrix_mul_operator.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef CMM_OPERATOR_CU_H
-#define CMM_OPERATOR_CU_H
-
-namespace cudaoperators
-{
-    void common_matrix_mul_operator_cu(const float *A, const float *B, float *C, int row, int col);
-}  // namespace operator
-#endif 
